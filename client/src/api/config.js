7c778ccc58468467f10f606f689581f3be541d7d
Jacob <jcn9284@gmail.com>
2026-05-12 20:28:14 +0300

    render.yaml moving to the root directory to solve publish problem

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,2 +1,3 @@
-export const API_BASE_URL = 'http://localhost:8000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+export const API_BASE_URL = `${API_URL}/api`;
 export const APP_VERSION = 'v1.1.0';
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -44,6 +45,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
     }),
   ],
   devServer: {
